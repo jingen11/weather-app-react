16ce0d473f10464cd460d222f5e1b2a1aec8ad21
Jing En <jingen11@hotmail.com>
2022-09-28 01:47:37 +0800

    ss

--- src/apis/openweather.js
+++ src/apis/openweather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const options = {
-  baseURL: "http://api.openweathermap.org/data/2.5",
+  baseURL: "https://api.openweathermap.org/data/2.5",
   params: {
     appid: "22a21cbe5a47afc1447f24012841816e",
     units: "metric",
